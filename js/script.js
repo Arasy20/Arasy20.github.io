be657909fdf7160070478078d54a3ff30ef7025a
Arasy20 <arasywana20@gmail.com>
2024-06-19 08:43:53 +0700

    penambahan js+button css

--- js/script.js
+++ js/script.js
@@ -1,1 +1,37 @@
-// ini js
+function tombolKonversi() {
+    let celciusInput = document.getElementById('celcius');
+    let farenheitInput = document.getElementById('farenheit');
+    let kalkulasiTextarea = document.getElementById('kalkulasi');
+  
+    let celciusValue = parseFloat(celciusInput.value);
+    let farenheitValue = parseFloat(celciusValue * 1.8) + 32;
+  
+    farenheitInput.value = farenheitValue;
+  
+    let kalkulasiText = `(${celciusValue}\u00B0C x 1.8) + 32`;
+    kalkulasiTextarea.value = kalkulasiText;
+  }
+
+function tombolReset() {
+    let celciusInput = document.getElementById('celcius');
+    let farenheitInput = document.getElementById('farenheit');
+    let kalkulasiTextarea = document.getElementById('kalkulasi');
+
+    celciusInput.value = "";
+    farenheitInput.value = "";
+    kalkulasiTextarea.value = "";
+}
+
+function tombolReverse() {
+    let farenheitInput = document.getElementById('farenheit');
+    let celciusInput = document.getElementById('celcius');
+    let kalkulasiTextarea = document.getElementById('kalkulasi');
+    
+    let farenheitValue = parseFloat(farenheitInput.value);
+    let celciusValue = (farenheitValue - 32) * 5/9;
+    
+    celciusInput.value = celciusValue.toFixed(2);
+    
+    let kalkulasiText = `(${farenheitValue}\u00B0F - 32) x 5/9`;
+    kalkulasiTextarea.value = kalkulasiText;
+}
